b6bb2104fdb564038cc9c8e6d7d4e4d84a94be60
WelsonAA <georgeezwelson@outlook.com>
2023-12-31 11:51:29 +0200

    Final Version

---
+++ IP-Project/Models/metaClassOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //b3ml add leh deh
+
+namespace WebApplication1.Models
+{
+
+
+    [MetadataType(typeof(metadata_Order))]
+    public partial class Order
+    {
+        //add new property
+    }
+    public class metadata_Order
+    {
+
+        public int Order_ID { get; set; }
+        [Display(Name = "User ID")]
+        
+        public Nullable<int> User_ID { get; set; }
+        [Display(Name = "Order Date")]
+
+        public Nullable<System.DateTime> Order_Date { get; set; }
+        [Display(Name = "Order Total Amount")]
+
+        public Nullable<double> Total_Amount { get; set; }
+        [Display(Name = "Order Payment method")]
+
+        public string Payment_Method { get; set; }
+
+    }
+}
--- IP-Project/Models/metaClassStores.cs
+++ IP-Project/Models/metaClassStores.cs
@@ -18,19 +18,24 @@ namespace WebApplication1.Models
     public class metadata_stores
     {
         public int store_id { get; set; }
+
         [Required(ErrorMessage = "Store name is required")]
         [Display(Name = "Store name" )]
         public string store_name { get; set; }
         public string store_image { get; set; }
+
         [Display(Name = "Store open time")]
 
         public string store_open_times { get; set; }
+
         [Display(Name = "Store close time")]
 
         public string store_close_times { get; set; }
+
         [Display(Name = "Store address")]
         [Required(ErrorMessage = "Store address is required")]
         public string store_address { get; set; }
+
         [Display(Name = "Store phone number")]
         [Required(ErrorMessage = "Store phone number is required")]
         public string store_Phonenumber { get; set; }
